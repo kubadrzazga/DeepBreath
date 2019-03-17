1edf9e1188203a058b586f7a07e669cb2d721f8d
underwood <pietrek224@gmail.com>
2019-03-17 21:42:59 +0100

    pobieranie danych stacji pomiarowych z api v.2

--- app/src/main/java/com/example/deepbreath/fetchData.java
+++ app/src/main/java/com/example/deepbreath/fetchData.java
@@ -2,6 +2,10 @@ package com.example.deepbreath;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,6 +16,9 @@ import java.net.URL;
 
 public class fetchData extends AsyncTask<Void,Void,Void>{
     String data="";
+    String dataParsed = "";
+    String singleParsed = "";
+
     @Override
     protected Void doInBackground(Void... voids) {
         try {
@@ -25,9 +32,23 @@ public class fetchData extends AsyncTask<Void,Void,Void>{
                 line = bufferedReader.readLine();
                 data = data + line;
             }
+            JSONArray JA = new JSONArray(data);
+            for(int i = 0; i < JA.length(); i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed = "id: " + JO.get("id") + "\n" +
+                        "stacja :" + JO.get("stationName") + "\n" +
+                        "gegrLat " + JO.get("gegrLat") + "\n" +
+                        "gegrLon " + JO.get("gegrLon") + "\n" +
+                        "ulica " + JO.get("addressStreet") + "\n" + "\n";
+                dataParsed = dataParsed + singleParsed;
+
+            }
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -38,6 +59,6 @@ public class fetchData extends AsyncTask<Void,Void,Void>{
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
 
-        MainActivity.data.setText(this.data);
+        MainActivity.data.setText(this.dataParsed);
     }
 }
